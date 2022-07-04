ffa1a4f2a471b229c19f419c2ea988be53be4851
AlexCorb95 <107132433+AlexCorb95@users.noreply.github.com>
2022-07-05 00:13:58 +0300

    Exercitii JS

--- full_site/scripts/exampleScript.js
+++ full_site/scripts/exampleScript.js
@@ -80,7 +80,8 @@ function schimb(){
     // var click= document.getElementsByid("tralala");
     var tit=document.getElementById("titluNews");
     tit.innerHTML = "NEWS";
-}
+}/*
+=========== facuta la Curs=========================
 function sumaUser(){
     var firstElem=document.getElementById("firstNumber");
     var secondElem=document.getElementById("secondNumber");
@@ -90,3 +91,43 @@ function sumaUser(){
     var results=document.getElementById("rezultatSuma");
     results.innerHTML=show;
 }
+===================================================
+*/
+//============Facuta dupa
+function getElemValue(){
+    var firstElem=document.getElementById("firstNumber");
+    var secondElem=document.getElementById("secondNumber");
+    var firstNumber=parseInt(firstElem.value);
+    var secondNumber=parseInt(secondElem.value);
+    var valueArray=[firstNumber,secondNumber];
+    return valueArray;
+}
+
+function sumElem(value){
+    results=0;
+    for(i=0;i<2;i++){
+        results+=value[i];
+    }
+    return results;
+}
+function prodElem(value){
+    results=1;
+    for(i=0;i<2;i++){
+        results=results*value[i];
+    }
+    return results;
+}
+function userSum(){
+    var valueArray=getElemValue();
+    var results=sumElem(valueArray);
+    var showUser=document.getElementById("rezultatSuma");
+    showUser.innerHTML=results;
+}
+function userProd(){
+    var valueArray=getElemValue();
+    var results=prodElem(valueArray);
+    var showUser=document.getElementById("rezultatProd");
+    showUser.innerHTML=results;
+}
+
+console.log(getElemValue());
