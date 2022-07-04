bfa4cd0f07e764b76d0fb95b226f718e3835a849
AlexCorb95 <107132433+AlexCorb95@users.noreply.github.com>
2022-07-04 21:06:39 +0300

    Exercitii in JS, onclick adaugat la cateva taguri pentru anumite functii

--- full_site/scripts/exampleScript.js
+++ full_site/scripts/exampleScript.js
@@ -19,14 +19,74 @@ while (i<=100){
 
 //Afisati toate nr cuprinse intre 50 si 150, divizibile cu 7
 for (i=50;i<=150;i++){      //Varianta cu for
-    if (i%5==0) {
+    if (i%7==0) {
         console.log(i);
     }
 }
 i=50
 while (i<=150){             //Varianta cu while
-    if (i%5==0) {
+    if (i%7==0) {
         console.log(i);
     }
     i++;
+}
+// Functii
+var a = 10;
+var b = 15;
+
+function suma(x,y){
+   return x+y;
+}
+
+var z= suma(3,4)
+console.log(suma(a,b))
+console.log(z)
+/*
+-functie cu 3 parametri a,b,c
+-functia returneaza un array care contine 
+toate nr de la a la b div cu c
+*/
+function divArray(a,b,c){
+    var ar=[];
+    while(a<=b){
+        if(a%c==0){
+            ar.push(a);
+        }
+        a++;
+    }
+    return ar;
+}
+var z =divArray(50,150,2);
+console.log(z);
+// fibonacci seq
+function fibonacciSeq(a){
+    var ar=[0,1];
+    for(i=2;i<a;i++){
+        ar.push(ar[i-2]+ar[i-1]);
+    }
+    return ar;
+
+}
+
+var sad=fibonacciSeq(10);
+console.log(sad);
+
+function schimbaTitlu(){
+    var titlu = document.getElementById("titluNews");
+    titlu.innerHTML = "De ce dai click pe mine?";
+}
+
+function schimb(){
+    // var click= document.getElementsByid("tralala");
+    var tit=document.getElementById("titluNews");
+    tit.innerHTML = "NEWS";
+}
+function sumaUser(){
+    var firstElem=document.getElementById("firstNumber");
+    var secondElem=document.getElementById("secondNumber");
+    var nr1=parseInt(firstElem.value);
+    var nr2=parseInt(secondElem.value) ;
+    var show=suma(nr1,nr2);
+    var results=document.getElementById("rezultatSuma");
+    results.innerHTML=show;
 }
